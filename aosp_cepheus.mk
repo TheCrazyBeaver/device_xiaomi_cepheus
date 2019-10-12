29ee75006fd69da8087a08afd03f845b514c6b27
TheCrazyBeaver <47144753+TheCrazyBeaver@users.noreply.github.com>
2019-10-12 21:43:38 +0200

    evox-ify

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -1,3 +1,4 @@
+
 #
 # Copyright (C) 2019 The LineageOS Project
 #
@@ -7,19 +8,23 @@
 $(call inherit-product, device/xiaomi/cepheus/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_cepheus
+PRODUCT_NAME := aosp_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/cepheus/cepheus:9/PKQ1.181121.001/9.5.30:user/release-keys"
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES_EVO := true
+TARGET_GAPPS_ARCH := arm64
+
+BUILD_FINGERPRINT := "Xiaomi/cepheuseea/cepheus:9/PKQ1.181121.001/V10.2.27.0.PFAEUXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cepheus-user 9 PKQ1.181121.001 9.5.30 release-keys" \
+    PRIVATE_BUILD_DESC="cepheus-user 9 PKQ1.181121.001 V10.2.27.0 release-keys" \
     PRODUCT_NAME="cepheus" \
     TARGET_DEVICE="cepheus"
 
